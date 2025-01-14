eed57acff2e1fbd96a7b5ae261caf24dd4ef353c
William MacBook <wwilliamw24@gmail.com>
2025-01-13 21:20:43 -0800

    initial commit with home screen

---
+++ BillMate/BillMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillMateApp.swift
+//  BillMate
+//
+//  Created by william wu on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BillMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BillMateTests/BillMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  BillMateTests.swift
+//  BillMateTests
+//
+//  Created by william wu on 1/12/25.
+//
+
+import Testing
+@testable import BillMate
+
+struct BillMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
